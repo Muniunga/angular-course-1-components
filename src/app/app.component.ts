0c1f56a007b9f26fc81ef26258a4f037d87eafdc
Muniunga <muniunga2000@gmail.com>
2024-09-18 02:05:44 +0100

    Output in components- Eventos personalizados  e emição de eventos de saida de dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { COURSES } from '../db-data';
+
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +10,15 @@ import {COURSES} from '../db-data';
 })
 export class AppComponent {
 
-coreCourse = COURSES[0];
 
-rxjsCourse= COURSES[1];
-rgrxCourse= COURSES[2];
+  coreCourse = COURSES[0];
+
+  rxjsCourse = COURSES[1];
+  rgrxCourse = COURSES[2];
+
+
+  onCourseSelected(course:Course){
+    console.log("App component - click event bubled...", course);
+
+  }
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { COURSES } from 'src/db-data';
 import { Course } from '../model/course';
+
 
 @Component({
   selector: 'app-course-card',
@@ -12,8 +13,23 @@ import { Course } from '../model/course';
 export class CourseCardComponent implements OnInit {
   @Input()
   course: Course;
-  Constructor() { } 
+  @Output() courseSelected = new EventEmitter<Course>();
+  Constructor() { }
   ngOnInit() {
   }
+  onCourseViewed() {
+    console.log('Card component - button clicked...');
+    this.courseSelected.emit(this.course);
+  }
+
+  // @Output() courseSelected = new EventEmitter<Course>();-> Essa é a forma mais comum e direta de declarar um evento de saída no Angular. O nome do evento que será emitido será exatamente o mesmo nome da variável, ou seja, courseSelected.
+
+  // @Output('courseSelected')  courseEmitter = new EventEmitter<Course>(); ->Aqui, estamos renomeando o evento exposto para o componente pai usando a sintaxe @Output('courseSelected'). Isso significa que, mesmo que o nome da propriedade dentro do componente filho seja courseEmitter, o nome que será usado pelo componente pai para ouvir esse evento será courseSelected.
+  // -------------EXEMplo completo----------------
+  // onCourseViewed(){
+  //   console.log('Card component - button clicked...');
+  //   this.courseEmitter.emit(this.course);
+  // }
+
 
 }
